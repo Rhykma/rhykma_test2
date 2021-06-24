642ad8e4f92765f614aac1a8b0a9b368f10ffd14
minedgeekble <86029271+minedgeekble@users.noreply.github.com>
2021-06-24 09:19:33 +0900

    pxt.json, main.py 업데이트하기

---
+++ main.py
@@ -0,0 +1,8 @@
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.pause(500)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.pause(500)
+    basic.show_icon(IconNames.YES)
+    basic.pause(500)
+basic.forever(on_forever)
